ae241a457a24ce15574ab8ec15e3efb9f30dd8d9
Don Dang <105334355+dondangp@users.noreply.github.com>
2024-02-29 11:21:42 -0600

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Install required Python packages
+pip install numpy pandas plotly python-dotenv stocknews streamlit yfinance
+
+# Ensure the correct version of Streamlit is installed
+pip install streamlit --upgrade
